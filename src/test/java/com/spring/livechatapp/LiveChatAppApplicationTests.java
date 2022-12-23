cabba37029aeef040c4b3003da4faac2f2d76347
Ismail <ismailbak7@gmail.com>
2022-12-23 13:37:37 +0100

    add spring boot project  🍃

---
+++ src/test/java/com/spring/livechatapp/LiveChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.livechatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveChatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
